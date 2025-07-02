146a8eed409572febfc5ee7af60f8d02de900caf
bentleyminwa <bentleyminwa@gmail.com>
2025-07-02 13:34:46 +0300

    initial project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send('user signup route');
+};
+
+export const login = async (req, res) => {
+    res.send('user login route');
+};
+
+export const logout = async (req, res) => {
+    res.send('user logout route');
+};
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+import { connectDB } from './lib/db.js';
+import { authRouter } from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use('/api/auth', authRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+    connectDB();
+});
